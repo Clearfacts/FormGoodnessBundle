a3777152c67ce317eccdb435b0b3891a485c29a9
JeroenMe <jeroen@tactics.be>
2014-12-11 14:41:52 +0100

    provide refresh finished event

--- FormJsBundle/Resources/public/js/refresh.js
+++ FormJsBundle/Resources/public/js/refresh.js
@@ -11,12 +11,16 @@ $.fn.refreshContainer = function (url) {
         },
         success: function(html) {
             container.html(html);
+            container.trigger('refresh.finished');
         },
         error: function() {
             container.prepend('<div class="alert alert-warning"><a class="close" data-dismiss="alert" href="#">×</a>Something went wrong with refresh, this info may be outdated. Please refresh the page</div>');
+            container.trigger('refresh.finished');
         },
         always: function(){
             container.ttSpin('hide');
         }
-    })
+    });
+
+    return container;
 };
